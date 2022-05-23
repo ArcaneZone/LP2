c81af5d6af8737d75fe4cb90bc98073267e8bacb
Siddhi Anil Bairagi <87110763+ArcaneZone@users.noreply.github.com>
2022-05-23 19:20:59 +0530

    Add files via upload

--- RSA.cpp
+++ RSA.cpp
@@ -1,37 +1,73 @@
-#include<bits/stdc++.h>
-using namespace std;
-int gcd(int x,int y){
-    if(y==0)
-    return x;
+  #include <iostream>
+   #include <math.h>
+   using namespace std;
 
-    return gcd(y,x%y);
-}
+  // find gcd
+  int gcd(int a, int b)
+  {
+    int t;
+    while (1)
+    {
+        t = a % b;
+        if (t == 0)
+            return b;
+        a = b;
+        b = t;
+    }
+  }
 
-int main(){
-    int p=3;
-    int q=7;
-    int n=p*q;
-    int phi=(p-1)*(q-1);
-    int e=2;
-    while(e<phi){
-        if(gcd(e,phi)==1){
+  int main()
+  {
+    //2 random prime numbers
+    double p = 13;
+    double q = 11;
+    double n = p * q; //calculate n
+    double track;
+    double phi = (p - 1) * (q - 1); //calculate phi
+    
+    //public key
+    //e stands for encrypt
+    double e = 7;
+    
+    //for checking that 1 < e < phi(n) and gcd(e, phi(n)) = 1; i.e., e and phi(n) are coprime.
+    while (e < phi)
+    {
+        track = gcd(e, phi);
+        if (track == 1)
             break;
-        }
         else
             e++;
     }
-    cout<<phi<<" ";
-    cout<<e<<" ";
-    //int d=(1%phi)/e;
-    int k=2;
-    double d=(k*phi + 1) / e;
-    cout<<d<<" ";
-    int mssg=12;
-    int E=pow(mssg,e);
-    double encrypt=fmod(E,n);
-    int D=pow(encrypt,d);
-    double decrypt=fmod(D,n);
-    cout<<encrypt<<" ";
-    cout<<decrypt<<" ";
+    
+    //private key
+    //d stands for decrypt
+    //choosing d such that it satisfies d*e = 1 mod phi
+    double d1 = 1 / e;
+    double d = fmod(d1, phi);
+    double message = 109;
+    double c = pow(message, e); //encrypt the message
+    double m = pow(c, d);
+    
+    c = fmod(c, n);
+    m = fmod(m, n);
+
+    cout << "Original Message = " << message;
+    cout << "\n"
+         << "p = " << p;
+    cout << "\n"
+         << "q = " << q;
+    cout << "\n"
+         << "n = pq = " << n;
+    cout << "\n"
+         << "phi = " << phi;
+    cout << "\n"
+         << "e = " << e;
+    cout << "\n"
+         << "d = " << d;
+    cout << "\n"
+         << "Encrypted message = " << c;
+    cout << "\n"
+         << "Decrypted message = " << m;
+
     return 0;
-}
+  }
